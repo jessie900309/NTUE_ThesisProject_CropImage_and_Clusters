a181de268b5149e95afe7444b2febc72f1e5849e
jessieOuO <jessie60125@gmail.com>
2024-02-20 20:35:56 +0800

    [update] cropped img

--- main.py
+++ main.py
@@ -26,37 +26,36 @@
 # from tool.read_directory_files import list_dirs_in_directory, list_files_in_directory, check_and_create_directory, get_basename
 # from util.constants import square_width
 # all_frame_dir = 'output/frames'
-# for dir_name in list_dirs_in_directory(all_frame_dir):
-#     frame_dir = all_frame_dir + '/' + dir_name
+# for vid_name in list_dirs_in_directory(all_frame_dir):
+#     frame_dir = all_frame_dir + '/' + vid_name
 #     output_dir = f'output/frames_500x500/{get_basename(frame_dir)}'
 #     check_and_create_directory(output_dir)
 #     for frame_name in list_files_in_directory(frame_dir):
-#         frame_path = frame_dir + '/' + frame_name
-#         output_cropped_path = f'{output_dir}/{frame_name[:-4]}_cropped_500x500.jpg'
-#         image_crop_square(image_path=frame_path, output_path=output_cropped_path, pixel=square_width)
 #         loop_count = (list_files_in_directory(frame_dir)).index(frame_name)
+#         frame_path = frame_dir + '/' + frame_name
+#         output_cropped_path = f'{output_dir}/{vid_name}_{loop_count+1:03d}.jpg'
+#         image_crop_square(image_path=frame_path, output_path=output_cropped_path, pixel=square_width)
 #         if loop_count % 10 == 0:
 #             print(f"正在輸出第{loop_count}張frame: {output_cropped_path}")
 # print("done.")
 
 
-# 修正500x500位置
+# 修正區塊位置
 # R. right -> +x  L. left <- -x
 # D. down   v +y  U. up    ^ -y
 # ---
 # print("run image_crop_square() EDIT!!!")
 # from tool.image_crop import image_crop_square_edit
 # from tool.read_directory_files import list_files_in_directory, check_and_create_directory, get_basename
-# from util.constants import square_width
-# edit_vid_name = '1_6_0_C0207'
+# edit_vid_name = '0_3_0_C0185'
 # frame_dir = 'output/frames/' + edit_vid_name
 # output_dir = f'output/frames_500x500/{get_basename(frame_dir)}'
 # check_and_create_directory(output_dir)
 # for frame_name in list_files_in_directory(frame_dir):
-#     frame_path = frame_dir + '/' + frame_name
-#     output_cropped_path = f'{output_dir}/{frame_name[:-4]}_cropped_500x500.jpg'
-#     image_crop_square_edit(image_path=frame_path, output_path=output_cropped_path, pixel=square_width)
 #     loop_count = (list_files_in_directory(frame_dir)).index(frame_name)
+#     frame_path = frame_dir + '/' + frame_name
+#     output_cropped_path = f'{output_dir}/{edit_vid_name}_{loop_count+1:03d}.jpg'
+#     image_crop_square_edit(image_path=frame_path, output_path=output_cropped_path)
 #     if loop_count % 10 == 0:
 #         print(f"正在輸出第{loop_count}張frame: {output_cropped_path}")
 # print("done.")
@@ -64,26 +63,26 @@
 
 # 取得影像遮罩並計算最大連通區域的像素數(面積)
 # ---
-import csv
-from tool.mask_process import get_max_mask_area
-from tool.read_directory_files import list_dirs_in_directory, list_files_in_directory, get_basename
-from util.constants import upper_red, lower_red, upper_green, lower_green, upper_brown, lower_brown
-input_dir = 'output/frames_500x500_O/0_3_0_C0185/'
-
-for frame_img in list_dirs_in_directory(input_dir):
-    video_name = get_basename(input_dir)
-    output_path = f'output/{video_name}_mask_area.csv'
-    no = 0
-    with open(output_path, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['no', 'red', 'green', 'brown', 'total'])
-        for img_name in list_files_in_directory(input_dir+'red/'):
-            no += 1
-            red_mask_area = get_max_mask_area(frame_img, upper=upper_red, lower=lower_red)
-            green_mask_area = get_max_mask_area(frame_img, upper=upper_green, lower=lower_green)
-            brown_mask_area = get_max_mask_area(frame_img, upper=upper_brown, lower=lower_brown)
-            total = red_mask_area + green_mask_area + brown_mask_area
-            writer.writerow([no, red_mask_area, green_mask_area, brown_mask_area, total])
-            if no % 100 == 0:
-                print(f"正在計算{video_name}的第{no}張影像")
-print("done.")
+# import csv
+# from tool.mask_process import get_max_mask_area
+# from tool.read_directory_files import list_dirs_in_directory, list_files_in_directory, get_basename
+# from util.constants import upper_red, lower_red, upper_green, lower_green, upper_brown, lower_brown
+# input_dir = 'output/frames_500x500_O/0_3_0_C0185/'
+#
+# for frame_img in list_dirs_in_directory(input_dir):
+#     video_name = get_basename(input_dir)
+#     output_path = f'output/{video_name}_mask_area.csv'
+#     no = 0
+#     with open(output_path, 'w', newline='') as csvfile:
+#         writer = csv.writer(csvfile)
+#         writer.writerow(['no', 'red', 'green', 'brown', 'total'])
+#         for img_name in list_files_in_directory(input_dir+'red/'):
+#             no += 1
+#             red_mask_area = get_max_mask_area(frame_img, upper=upper_red, lower=lower_red)
+#             green_mask_area = get_max_mask_area(frame_img, upper=upper_green, lower=lower_green)
+#             brown_mask_area = get_max_mask_area(frame_img, upper=upper_brown, lower=lower_brown)
+#             total = red_mask_area + green_mask_area + brown_mask_area
+#             writer.writerow([no, red_mask_area, green_mask_area, brown_mask_area, total])
+#             if no % 100 == 0:
+#                 print(f"正在計算{video_name}的第{no}張影像")
+# print("done.")
